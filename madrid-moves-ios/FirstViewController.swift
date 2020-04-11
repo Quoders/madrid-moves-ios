9847228e7565cb1694fe28224aebf003c7899987
David Guerrero <david.guerrero@yoti.com>
2020-04-11 18:11:01 +0100

    Initial Commit

---
+++ madrid-moves-ios/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  madrid-moves-ios
+//
+//  Created by David Guerrero on 11/04/2020.
+//  Copyright © 2020 David Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ madrid-moves-ios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  madrid-moves-ios
+//
+//  Created by David Guerrero on 11/04/2020.
+//  Copyright © 2020 David Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
